338b2a5f5aaf09262e2ab525e66d1332ab124c47
abhijit <kamathabhijit2002@gmail.com>
2024-10-17 22:20:02 -0400

    simple POST endpoint

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	// "fmt"
-	"io"
-	"log"
+	"fmt"
+	"errors"
 	"net/http"
+	"encoding/json"
 )
 
 func main(){
 	
-	receiveMail := func(w http.ResponseWriter, _ *http.Request){
-		io.WriteString(w,"Hello from receiveMail");
+	receiveMail := func(w http.ResponseWriter, request *http.Request){
+		if request.Method == "GET"{
+			w.WriteHeader(http.StatusBadRequest);
+			w.Header().Set("Content-Type", "application/json")
+			jsonEncoder := json.NewEncoder(w)
+			jsonEncoder.Encode("This is a POST endpoint to handle contact form submissions")
+		}else if request.Method == "POST"{
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			jsonEncoder := json.NewEncoder(w)
+			jsonEncoder.Encode("Returning json input?")
+		}	
 	}
 	http.HandleFunc("/receive", receiveMail)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed){
+		fmt.Printf("server closed\n")
+	}else if err != nil {
+		fmt.Printf("error: %v\n",err)
+	}
 }
 
 
